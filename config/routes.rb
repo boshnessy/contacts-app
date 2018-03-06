e2863a59a3ff15698fc0f6728a3348d75c7a8a19
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-05 21:30:19 -0600

    Add faker gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/contact_list' => 'contacts#show_my_contacts'
+  get '/all_contacts' => 'contacts#all_contacts'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# Add the faker gem to a student’s app and seed 100 users (working on file)
+100.times do
+  contact1 = Contact.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number)
+
+  contact1.save
+end
+
