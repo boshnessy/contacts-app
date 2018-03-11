6520eb7f3c76d5c44829ffdadd724378e7177346
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-11 12:28:31 -0500

    add model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,25 @@
 class Contact < ApplicationRecord
+  def as_json
+    {
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      full_name: full_name,
+      email: email,
+      phone_number: japanese_phone_number,
+      updated_at: friendly_updated_at
+    }
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%m/%d/%Y")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+  end
+
+  def japanese_phone_number
+    japanese_phone_number = "+81 #{phone_number}"
+  end
 end
