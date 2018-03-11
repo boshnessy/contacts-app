720ee9a70d8cd5aa50528586e5aed2a3ebed2899
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-11 11:10:27 -0500

    made minor updates

--- app/controllers/v2/contacts_controller.rb
+++ app/controllers/v2/contacts_controller.rb
@@ -1,25 +1,4 @@
 class V2::ContactsController < ApplicationController
-  def show_my_contacts
-    contact1 = Contact.first
-
-    # render json: {first_name: contact1.first_name, last_name: contact1.last_name, email: contact1.email, phone_number: contact1.phone_number}
-    render json: contact1.as_json # same but also includes id, created at, updated at
-  end
-
-  def all_contacts
-    contacts = Contact.all
-    #make an empty array
-    contacts_hashes = []
-    #loop through all contacts  
-    contacts.each do |contact|
-      #make a hash for each contact
-      the_contact_hash = {firstName: contact.first_name, lastName: contact.last_name, email: contact.email, phoneNumber: contact.phone_number}
-      #shovel hash into array
-      contacts_hashes << the_contact_hash
-    end
-    render json: contacts_hashes # already a hash -> don't need hash brackets
-  end
-
   def index
     contacts = Contact.all
 
@@ -34,15 +13,15 @@ class V2::ContactsController < ApplicationController
   end
 
   def create
-    contact1 = Contact.new(
+    contact = Contact.new(
       first_name: params[:input_first_name], 
       last_name: params[:input_last_name], 
       email: params[:input_email], 
       phone_number: params[:input_phone_number]
     )
-    contact1.save
+    contact.save
 
-    render json: contact1.as_json
+    render json: contact.as_json
   end
 
   def update
@@ -54,6 +33,12 @@ class V2::ContactsController < ApplicationController
       email: params[:input_email], 
       phone_number: params[:input_phone_number]
     )
+    # contact.first_name = params[:input_first_name]
+    # contact.last_name = params[:input_last_name]
+    # contact.email = params[:input_email]
+    # contact.phone_number = params[:input_phone_number]
+    # same but need to save after
+    # contact.save
 
     render json: contact.as_json
   end
--- frontend.rb
+++ frontend.rb
@@ -1,30 +1,5 @@
 require 'unirest'
 
-# # one contact
-# p "Here is your first contact:"
-# response = Unirest.get("http://localhost:3000/v1/contact_list")
-
-# contact = response.body
-
-# p "first name: #{contact["first_name"]}"
-# p "last name: #{contact["last_name"]}"
-# p "email: #{contact["email"]}"
-# p "phone number: #{contact["phone_number"]}"
-
-
-# # all contacts
-# response = Unirest.get("http://localhost:3000/v1/all_contacts")
-# contacts = response.body
-
-# # loop through contacts, print out each
-# contacts.each do |contact|
-#   p "first name: #{contact["first_name"]}"
-#   p "last name: #{contact["last_name"]}"
-#   p "email: #{contact["email"]}"
-#   p "phone number: #{contact["phone_number"]}"
-#   p "------------------------"
-# end
-
 # CREATE
 # p "Enter a first_name:"
 # first_name = gets.chomp
@@ -72,4 +47,4 @@ p "Enter the id of the contact you wish to delete"
 contact_id = gets.chomp
 
 response = Unirest.delete("localhost:3000/v2/contacts/#{contact_id}")
-p "You successfully deleted your contact."
+puts JSON.pretty_generate(response.body)
