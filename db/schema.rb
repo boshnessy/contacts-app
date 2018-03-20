124d12c552804a4d4d1ed6da603710fc60154d70
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-19 19:27:31 -0500

    associate contacts and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319234622) do
+ActiveRecord::Schema.define(version: 20180320002528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180319234622) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
